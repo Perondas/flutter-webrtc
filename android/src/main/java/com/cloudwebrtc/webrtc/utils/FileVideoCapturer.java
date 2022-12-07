6e4c0a559c7a853bb0a518b1713e2b551c64201f
Perondas <Pperondas@gmail.com>
2022-12-07 12:44:13 +0100

    Fixed colors of bmp streaming

--- android/src/main/java/com/cloudwebrtc/webrtc/utils/FileVideoCapturer.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/utils/FileVideoCapturer.java
@@ -13,6 +13,9 @@ package com.cloudwebrtc.webrtc.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.os.SystemClock;
 
 import android.util.Log;
@@ -20,8 +23,10 @@ import android.util.Log;
 import org.webrtc.CapturerObserver;
 import org.webrtc.JavaI420Buffer;
 import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.TextureBufferImpl;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoFrame;
+import org.webrtc.YuvConverter;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -44,6 +49,7 @@ public class FileVideoCapturer implements VideoCapturer {
     private Timer timer = null;
 
     private Bitmap bmp;
+    private SurfaceTextureHelper surfaceTextureHelper;
 
 
     public void setBmp(byte[] bytes) {
@@ -53,18 +59,31 @@ public class FileVideoCapturer implements VideoCapturer {
     public void tick() {
         if (bmp == null) return;
         final long captureTimeNs = TimeUnit.MILLISECONDS.toNanos(SystemClock.elapsedRealtime());
-        final JavaI420Buffer buffer = JavaI420Buffer.allocate(bmp.getWidth(), bmp.getHeight());
+        YuvConverter yuvConverter = new YuvConverter();
+        int[] textures = new int[1];
+        GLES20.glGenTextures(0, textures, 0);
+        TextureBufferImpl  buffer = new TextureBufferImpl(bmp.getWidth(), bmp.getHeight(), VideoFrame.TextureBuffer.Type.RGB, textures[0], new Matrix(), surfaceTextureHelper.getHandler(), yuvConverter, null);
+        Bitmap flippedBitmap = createFlippedBitmap(bmp, true, false);
+        surfaceTextureHelper.getHandler().post(() -> {
+            if (flippedBitmap != null) {
+                GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+                GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+                GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, flippedBitmap, 0);
 
-        bitmapToI420(bmp,buffer);
-        VideoFrame frame = new VideoFrame(buffer, 0 /* rotation */, captureTimeNs);
-        capturerObserver.onFrameCaptured(frame);
-        frame.release();
+                VideoFrame.I420Buffer i420Buf = yuvConverter.convert(buffer);
+
+                VideoFrame videoFrame = new VideoFrame(i420Buf, 180, captureTimeNs);
+                capturerObserver.onFrameCaptured(videoFrame);
+                videoFrame.release();
+            }
+        });
     }
 
 
     @Override
     public void initialize(SurfaceTextureHelper surfaceTextureHelper, Context context, CapturerObserver capturerObserver) {
         this.capturerObserver = capturerObserver;
+        this.surfaceTextureHelper = surfaceTextureHelper;
     }
 
     @Override
@@ -101,37 +120,31 @@ public class FileVideoCapturer implements VideoCapturer {
         return false;
     }
 
-    private static void bitmapToI420(Bitmap src, JavaI420Buffer dest) {
+    private  void bitmapToI420(Bitmap src, JavaI420Buffer dest) {
         int width = src.getWidth();
         int height = src.getHeight();
-
-        if(width != dest.getWidth() || height != dest.getHeight())
+        if (width != dest.getWidth() || height != dest.getHeight())
             return;
-
         int strideY = dest.getStrideY();
         int strideU = dest.getStrideU();
         int strideV = dest.getStrideV();
         ByteBuffer dataY = dest.getDataY();
         ByteBuffer dataU = dest.getDataU();
         ByteBuffer dataV = dest.getDataV();
-
-        for(int line = 0; line < height; line++) {
-            if(line % 2 == 0) {
-                for (int x = 0; x < width - 1; x += 2) {
+        for (int line = 0; line < height; line++) {
+            if (line % 2 == 0) {
+                for (int x = 0; x < width; x += 2) {
                     int px = src.getPixel(x, line);
                     byte r = (byte) ((px >> 16) & 0xff);
                     byte g = (byte) ((px >> 8) & 0xff);
                     byte b = (byte) (px & 0xff);
-
                     dataY.put(line * strideY + x, (byte) (((66 * r + 129 * g + 25 * b) >> 8) + 16));
                     dataU.put(line / 2 * strideU + x / 2, (byte) (((-38 * r + -74 * g + 112 * b) >> 8) + 128));
                     dataV.put(line / 2 * strideV + x / 2, (byte) (((112 * r + -94 * g + -18 * b) >> 8) + 128));
-
                     px = src.getPixel(x + 1, line);
                     r = (byte) ((px >> 16) & 0xff);
                     g = (byte) ((px >> 8) & 0xff);
                     b = (byte) (px & 0xff);
-
                     dataY.put(line * strideY + x, (byte) (((66 * r + 129 * g + 25 * b) >> 8) + 16));
                 }
             } else {
@@ -140,10 +153,18 @@ public class FileVideoCapturer implements VideoCapturer {
                     byte r = (byte) ((px >> 16) & 0xff);
                     byte g = (byte) ((px >> 8) & 0xff);
                     byte b = (byte) (px & 0xff);
-
                     dataY.put(line * strideY + x, (byte) (((66 * r + 129 * g + 25 * b) >> 8) + 16));
                 }
             }
+        }
+    }
+    private static Bitmap createFlippedBitmap(Bitmap source, boolean xFlip, boolean yFlip) {
+        try {
+            Matrix matrix = new Matrix();
+            matrix.postScale(xFlip ? -1 : 1, yFlip ? -1 : 1, source.getWidth() / 2f, source.getHeight() / 2f);
+            return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+        } catch (Exception e) {
+            return null;
         }
     }
 }
